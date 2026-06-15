4ab0f2e1f43de01c8cd55cd72c02021bc3745fe9
oleksandr <oleksandr.k.kodehode@gmail.com>
2026-06-15 13:22:15 +0200

    wtf

--- main.js
+++ main.js
@@ -9,3 +9,29 @@ const createPlayer = (map, x, y, img) => {
     return mainFig;
 };
 createPlayer(map, 10, 10, "./assets/player/player-default.png");
+
+// const player = new Person("./assets/player/player-default.png", null, 100, 100);
+
+// const keys = {};
+
+// document.addEventListener("keydown", (e) => {
+//     keys[e.code] = true;
+// });
+
+// document.addEventListener("keyup", (e) => {
+//     keys[e.code] = false;
+// });
+
+function gameLoop() {
+    if (keys["KeyW"]) player.y -= player.speed;
+    if (keys["KeyS"]) player.y += player.speed;
+    if (keys["KeyA"]) player.x -= player.speed;
+    if (keys["KeyD"]) player.x += player.speed;
+
+    player.entity.style.left = player.x + "px";
+    player.entity.style.top = player.y + "px";
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
