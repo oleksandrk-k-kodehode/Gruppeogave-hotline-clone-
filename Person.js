2148847b349df9fd4346d47cef929c635448a45b
dk99kodehode <dk99.kodehode@gmail.com>
2026-06-18 10:41:28 +0200

    added directional bullet

--- Person.js
+++ Person.js
@@ -60,12 +60,13 @@ class Person {
 
   #bulletBuild(x, y, img, speed, damage, width, height) {
     const bullet = document.createElement("div");
-    bullet.className = "bullets";
-
-    const bulletImg = new Image();
-    bulletImg.src = "./assets/bullet.png";
+    bullet.className = "bullet";
+    bullet.style.position = "absolute";
     bullet.style.width = `${width}px`;
     bullet.style.height = `${height}px`;
+
+    const bulletImg = new Image();
+    bulletImg.src = img;
 
     bullet.append(bulletImg);
     return bullet;
@@ -94,9 +95,34 @@ class Person {
   }
 
   shoot() {
-    player.append(this.#bulletBuild());
-    console.log("pew pew");
-    return;
+    const centerX = this.x + this.width / 2;
+    const centerY = this.y + this.width / 2;
+
+    const bulletElem = this.#bulletBuild(
+      centerX,
+      centerY,
+      "./assets/bullet.png",
+      8,
+      10,
+      10,
+      10,
+    );
+
+    bulletElem.style.position = "absolute";
+    bulletElem.style.left = centerX + "px";
+    bulletElem.style.top = centerY + "px";
+
+    const angle = Math.atan2(this.mouseY - centerY, this.mouseX - centerX);
+    const bulletSpeed = 10;
+
+    bulletElem.currentX = centerX;
+    bulletElem.currentY = centerY;
+    bulletElem.vx = Math.cos(angle) * bulletSpeed;
+    bulletElem.vy = Math.sin(angle) * bulletSpeed;
+
+    const mapElement = document.getElementById("map");
+    if (mapElement) mapElement.append(bulletElem);
+    return bulletElem;
   }
 }
 
