ca12e328010fc0fa6fdc9e7d178d87a26fd66141
dk99kodehode <dk99.kodehode@gmail.com>
2026-06-20 01:38:33 +0200

    enemy class added, and syntax errors in spwaner fixed

--- enemy.js
+++ enemy.js
@@ -1,37 +1,19 @@
-import { Seekbehaviour } from "./Seekbehaviour.js";
-import { map } from "./main.js";
-import { Person } from "./Person.js";
-import { EnemySpawner } from "./Enemyspawner.js";
-
-const enemy = document.createElement("div");
-enemy.classList = enemy;
-enemy.imgSrc = "./assets/enemy/enemy-normal.png";
-map.append(enemy);
-
-//----data er stored i Enemydata//
-class Enemy {
-  constructor(data, behaviour) {
-    this.data = data;
-    this.behaviour = behaviour;
-    this.x = 0;
-    this.y = 0;
-    this.width = data.width;
-    this.speed = data.speed;
-    this.damage = data.damage;
-    this.health = data.health;
-    this.behaviour = behaviour;
-  }
-  spawn(x, y) {
+export class Enemy {
+  constructor(x, y, width, height, speed, behaviour, image) {
     this.x = x;
     this.y = y;
-    this.health = this.data.health;
-    this.active = true;
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+    this.behaviour = behaviour;
+    this.image = image;
   }
-  reset() {
-    this.active = false;
-    this.health = this.data.health;
-  }
-  update() {
+
+  update(dt, player) {
     this.behaviour.update(this, dt, player);
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 }
