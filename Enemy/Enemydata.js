2f68fc3d381943f7965052c4a2860c944e93df5e
dk99kodehode <dk99.kodehode@gmail.com>
2026-06-24 13:15:42 +0200

    changed background image for game over

--- Enemy/Enemydata.js
+++ Enemy/Enemydata.js
@@ -4,6 +4,7 @@ export const enemyData = {
     height: 48,
     behaviourType: "seek",
     image: "Assets/enemy/mob/mob-regular.png",
+    death: "Assets/death.png",
     bulletImg: "Assets/enemy/mob/mob-bullet.png",
   },
   Boss: {
@@ -11,6 +12,7 @@ export const enemyData = {
     height: 48,
     behaviourType: "seek",
     image: "Assets/enemy/bossmob/bossmob-regular.png",
+    death: "Assets/death.png",
     bulletImg: "Assets/enemy/bossmob/mob-boss-bullet.png",
   },
 };
