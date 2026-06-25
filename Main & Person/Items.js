f2ca8543115df8077e6fb1ce5241d5d8789e9920
oleksandr <oleksandr.k.kodehode@gmail.com>
2026-06-25 11:52:48 +0200

    fixedd gameloop stop

--- Main & Person/Items.js
+++ Main & Person/Items.js
@@ -1,19 +1,28 @@
 export const items = [
-  {
-    name: Kåresknekkebrød,
-    health: 3,
-    bulletCount: +2,
-  },
-  {
-    name: OleksandersPasta,
-    speed: 12,
-    bulletCount: -1,
-  },
-  {
-    name: HealthPotion,
-    health: 5,
-  },
-  {
-    name: GrandmasBazooka,
-  },
+    {
+        name: Kåresknekkebrød,
+        health: 3,
+        bulletCount: +2,
+    },
+    {
+        name: Danielselectrolytes,
+        health: 67,
+        bulletCount: +42,
+        duration: 5,
+    },
+    {
+        name: OleksandersPasta,
+        speed: 12,
+        bulletCount: -1,
+    },
+    {
+        name: HealthPotion,
+        health: 5,
+    },
+    {
+        name: GrandmasBazooka,
+    },
+    {
+        name: GrandmasAshes,
+    },
 ];
