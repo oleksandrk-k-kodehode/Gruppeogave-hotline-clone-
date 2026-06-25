85225c6af91d802b6bca9c0518bd8975b35b23e6
dk99kodehode <dk99kodehode@gmail.com>
2026-06-25 17:14:56 +0200

    local storage to score added for gameoverlay

--- Gameover.js
+++ Gameover.js
@@ -2,11 +2,12 @@ export function gameOver() {
   const gameOver = document.getElementById("game-over-overlay");
   gameOver.style.display = "block";
 
+  const score = document.getElementById("total-score");
+  score.textContent = localStorage.getItem("lastScore") || 0;
+  window.killElement.textContent = "";
+
   const reset = document.querySelector(".reset");
   reset.addEventListener("click", () => {
     window.location.reload();
   });
-
-  const score = document.getElementById(".total-score");
-  window.killElement.textContent = "";
 }
