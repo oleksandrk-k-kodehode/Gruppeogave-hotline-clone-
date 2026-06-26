9c9eb1e7e5f468b561b9380cf83b2b4ef5b1bc45
dk99kodehode <dk99.kodehode@gmail.com>
2026-06-26 12:04:25 +0200

    the work continues

--- Main & Person/Items.js
+++ Main & Person/Items.js
@@ -3,44 +3,26 @@ export const items = [
     name: Kåresknekkebrød,
     health: 3,
     bulletCount: +2,
+
+    cost: 10,
+    image: "Assets/items/kåres-knekkebrød.png",
   },
   {
     name: OleksandersPasta,
     speed: 12,
-    bulletCount: -1,
+    bulletCount: 67,
+
+    cost: 50,
+    image: "Assets/items/oleksander-pasta",
   },
   {
-    name: HealthPotion,
-    health: 5,
+    name: GrandmasAshes,
+    speed: -33,
+
+    bulletSpeed: "back in my day",
+    bulletCount: 400,
+
+    cost: 120,
+    image: "Assets/items/grandmas-ashes.png",
   },
-  {
-    name: GrandmasBazooka,
-  },
-  {},
-    {
-        name: Kåresknekkebrød,
-        health: 3,
-        bulletCount: +2,
-    },
-    {
-        name: Danielselectrolytes,
-        health: 67,
-        bulletCount: +42,
-        duration: 5,
-    },
-    {
-        name: OleksandersPasta,
-        speed: 12,
-        bulletCount: -1,
-    },
-    {
-        name: HealthPotion,
-        health: 5,
-    },
-    {
-        name: GrandmasBazooka,
-    },
-    {
-        name: GrandmasAshes,
-    },
 ];
